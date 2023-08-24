0acdafe940681caf173c6eaa1025ffa2f46a97b4
Mohammad Yaqoob Yousufi <mohdyaqoob.yousufi@gmail.com>
2023-08-24 19:58:41 +0500

    lesson 24 - 8 -2018

--- members/urls.py
+++ members/urls.py
@@ -2,32 +2,34 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-    path('members/members/', views.members, name='all_members.html'),
-
-    path('members/myfirst/', views.myfirst, name='myfirst.html'),
-
-    path('members/profile/', views.profile, name='profile.html'),
-
-    path('members/contact/', views.contact, name='contact.html'),
-
-    path('members/about/', views.about, name='about.html'),
-
-    path('members/alifmobile', views.alifmobile, name='alifmobile'),
     
-    path('members/alifonline', views.alifonline, name='alifonline'),
-    
-    path('members/alifshop', views.alifshop, name='alifshop'),
-    
-    path('members/carfinance', views.carfinance, name='carfinance'),
-    
-    path('members/deposits', views.deposits, name='deposits'),
-    
-    path('members/salam', views.salam, name='salam'),
-    
-    path('members/transfers', views.transfers, name='transfers'),
+    path('', views.main, name='main'),
 
-    path('members/visa', views.visa, name='visa'),
+    path('members/', views.members, name='all_members.html'),
+
+    path('myfirst/', views.myfirst, name='myfirst.html'),
+
+    path('profile/', views.profile, name='profile.html'),
+
+    path('contact/', views.contact, name='contact.html'),
+
+    path('about/', views.about, name='about.html'),
+
+    path('alifmobile', views.alifmobile, name='alifmobile'),
+    
+    path('alifonline', views.alifonline, name='alifonline'),
+    
+    path('alifshop', views.alifshop, name='alifshop'),
+    
+    path('carfinance', views.carfinance, name='carfinance'),
+    
+    path('deposits', views.deposits, name='deposits'),
+    
+    path('salam', views.salam, name='salam'),
+    
+    path('transfers', views.transfers, name='transfers'),
+
+    path('visa', views.visa, name='visa'),
     
     path('members/details/<int:id>', views.details, name='details'),
 
--- members/views.py
+++ members/views.py
@@ -70,7 +70,9 @@ def details(request, id):
   }
   return HttpResponse(template.render(context, request))
 
-
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
 
 
 
