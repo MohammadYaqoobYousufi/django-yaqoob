f6ca2f726c829e1f0b74883802c8c9007bdc4da6
Mohammad Yaqoob Yousufi <mohdyaqoob.yousufi@gmail.com>
2023-08-26 19:29:07 +0500

    new files added

--- django_firstproject_yaqoob/settings.py
+++ django_firstproject_yaqoob/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(a^*ct3gq3z-xx4kio)&ox89w+u^vme070uyd!)1jqxy6^1!h(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
