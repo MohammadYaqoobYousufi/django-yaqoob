1808e812990241d0b282abab1ad2c9a5d1fc98f9
Mohammad Yaqoob Yousufi <mohdyaqoob.yousufi@gmail.com>
2023-08-29 20:02:09 +0500

    form added

--- members/views.py
+++ members/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .forms import MemberForm
 from .models import Member
 
 
 def members(request):
+  form = MemberForm()
+
+  if request.method == 'POST':
+    form = MemberForm(request.POST)
+    if form.is_valid():
+      form.save()
+
   mymembers = Member.objects.all().values()
   template = loader.get_template('all_members.html')
   context = {
     'mymembers': mymembers,
+    'form': form,
   }
   return HttpResponse(template.render(context, request))
 
